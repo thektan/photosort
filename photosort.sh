e3ca40659901d7e047b3ba176e60a90cb68354c1
Kevin Tan <thektan@gmail.com>
2017-02-12 18:52:35 -0800

    Add echo statements to know what's currently happening as the script runs

--- photosort.sh
+++ photosort.sh
@@ -1,5 +1,14 @@
 # Sort files from a single directory into separate directories by year and month.
+local total_count=$(ls -l | grep -v ^l | wc -l);
+total_count=$((total_count-1))
+
+let count=0;
+
 for file in *; do
+	let "count++";
+
+	echo -n "\rCurrently moving: $count/$total_count";
+
 	local created=$(GetFileInfo -d $file);
 
 	local month=$(date -jf "%m/%d/%Y %H:%M:%S" $created +%m)
@@ -9,3 +18,5 @@ for file in *; do
 
 	mv $file $year/$month/$file;
 done
+
+echo "\n$total_count files sorted"
